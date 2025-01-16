260de2b9cd43d9059f537e47b2e1d0cc47cf43e6
coderyigit6 <coderyigit@outlook.com>
2025-01-16 14:44:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("message").innerText = "this message is your gift!(you clicked the button so you got this message)";
+});
